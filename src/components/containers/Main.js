9675b028370cc3b94608c0b7158c2f0c92048d98
Afam Olie <mikeolie@mikesImac.domain.com>
2020-06-06 15:15:36 -0700

    added section for featured and side contetn

--- src/components/containers/Main.js
+++ src/components/containers/Main.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../containers/Navbar";
-
+import { Container, Row, Col } from "reactstrap";
 const Main = () => (
   <div className="home--content">
     <Navbar />
-    Home Content
+    <Container>
+      <Row>
+        <Col md={8}>Featured Content</Col>
+        <Col md={4}>Side Content</Col>
+      </Row>
+    </Container>
   </div>
 );
 
