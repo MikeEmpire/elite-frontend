8c9bbfc1d7e9f67c65abfd1d307ae86f29304eac
jtaylor6 <jarrede20@gmail.com>
2020-06-30 11:14:46 -0400

    updated colors & fonts

--- src/components/presentation/Splash.js
+++ src/components/presentation/Splash.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Splash = () => (
   <div className="splash--container">
-    <h1 className="site-title font-1">Elite</h1>
+    <h1 className="site-title font-1">ELITE</h1>
     <h4 className="font-2" id="foreign-text">
       /e'lét,å'lét/
     </h4>
@@ -12,7 +12,7 @@ const Splash = () => (
       rest of a group or society
     </h6>
     <Link to="/main">
-      <h6 className="font-1" style={{ opacity: ".7" }}>Click here to continue</h6>
+      <h5 className="enter font-1" style={{ opacity: ".7" }}>Click here to continue</h5>
     </Link>
   </div>
 );
